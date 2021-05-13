add870558c0b276d6ae7be0f1a82e77edfc712bb
Yuvrajhere <yuvrajisbest13@gmail.com>
2021-05-13 13:58:58 +0530

    Navbar Comp done & used in LandingPage, Login and Signup page

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,9 +1,14 @@
 import "./Login.css";
 
+import Navbar from "../smallerComponents/Navbar";
+
 const Login = () => {
   return (
-    <div className="Login">
-      Login
+    <div className="Login container-1">
+      <Navbar />
+      <main className="container-2">
+        Login
+      </main>
     </div>
   )
 }
--- src/components/smallerComponents/Navbar/index.js
+++ src/components/smallerComponents/Navbar/index.js
@@ -1,18 +1,65 @@
 import "./Navbar.css";
 
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
 // images
 import Logo from "../../../assets/images/logo.png";
 import menuIcon from "../../../assets/images/menu-icon.png";
+import menuCloseIcon from "../../../assets/images/menu-close-icon.png";
+import homeIcon from "../../../assets/images/home-icon.png";
+import loginIcon from "../../../assets/images/login-icon.png";
+import signupIcon from "../../../assets/images/signup-icon.png";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <div className="Navbar container-2">
       <img src={Logo} alt="Brand Logo" />
-      <div>
-        <img src={menuIcon} alt="menu button" />
+      <div className="menu">
+        <img
+          className="menu-close-btn"
+          onClick={() => {
+            document.getElementsByClassName("menu")[0].style.width = "0";
+            setIsMenuOpen(false);
+          }}
+          src={menuCloseIcon}
+        />
+        <ul>
+          <li>
+            <NavLink to="/" activeClassName="active" exact>
+              <img src={homeIcon} alt="home" />
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/login" activeClassName="active" exact>
+              <img src={loginIcon} alt="login" />
+              Login
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/signup" activeClassName="active" exact>
+              <img src={signupIcon} alt="signup" />
+              Signup
+            </NavLink>
+          </li>
+        </ul>
       </div>
+
+      {!isMenuOpen && (
+        <img
+          onClick={() => {
+            document.getElementsByClassName("menu")[0].style.width = "70%";
+            setIsMenuOpen(true);
+          }}
+          src={menuIcon}
+          className="menu-btn"
+        ></img>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
