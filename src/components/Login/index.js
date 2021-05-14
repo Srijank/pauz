a7523e70e069065c90423437595ff59588ebda59
Yuvrajhere <yuvrajisbest13@gmail.com>
2021-05-14 19:36:03 +0530

    Added Login feature in Login comp

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,15 +1,28 @@
 import "./Login.css";
 
-import Navbar from "../smallerComponents/Navbar";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { showAlert, startLoading, stopLoading } from "../../actions/index";
+import { connect } from "react-redux";
+import axios from "axios";
 
+import Navbar from "../smallerComponents/Navbar";
 import Button from "../smallerComponents/Button";
 import Input from "../smallerComponents/Input";
-import { useState } from "react";
 
 import LoginImage1 from "../../assets/images/login-img-1.png";
-import { Link } from "react-router-dom";
 
-const Login = () => {
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = {
+  showAlert: showAlert,
+  startLoading: startLoading,
+  stopLoading: stopLoading,
+};
+
+const Login = (props) => {
   const [loginDetails, setLoginDetails] = useState({
     email: "",
     password: "",
@@ -20,6 +33,29 @@ const Login = () => {
       ...loginDetails,
       [e.target.name]: e.target.value,
     });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.startLoading();
+    axios
+      // .post(`${process.env.REACT_APP_API_URL}/users/login`, signinData)
+      .post(`http://localhost:5000/api/users/login`, loginDetails)
+      .then(
+        (response) => {
+          localStorage.setItem("token", response.data.token);
+          props.stopLoading();
+          window.location.reload(false);
+        },
+        (error) => {
+          props.stopLoading();
+          if (error.response) {
+            props.showAlert(error.response.data.message);
+          } else {
+            props.showAlert(error.message);
+          }
+        }
+      );
   };
 
   return (
@@ -33,7 +69,7 @@ const Login = () => {
         </p>
 
         <img src={LoginImage1} alt="Friends in windows" width="100%" height="100%" />
-        <form className="fillform">
+        <form onSubmit={handleSubmit} className="fillform">
           <Input
             type="email"
             label="Email"
@@ -68,4 +104,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
