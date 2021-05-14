918de0cf675f3e50bff2bdfe148bd0d52cf6bfc5
Yuvrajhere <yuvrajisbest13@gmail.com>
2021-05-14 21:28:42 +0530

    Add PostList, Post, AppNavbar

---
+++ src/components/PostList/index.js
@@ -0,0 +1,15 @@
+import "./PostList.css";
+
+import Post from "../Post";
+
+const PostList = () => {
+  return (
+    <div className="PostList">
+      <Post />
+      <Post />
+      <Post /> 
+    </div>
+  )
+}
+
+export default PostList;
